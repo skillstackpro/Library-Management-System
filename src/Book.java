bee0d7510a08842af8578095aab1901635987b60
skillsstackpro <rohankataria48@gmail.com>
2026-02-11 21:24:28 +0500

    Add Book class

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+class Book {
+    int id;
+    String title;
+    String author;
+    int copies;
+
+    public Book(int id, String title, String author, int copies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.copies = copies;
+    }
+}

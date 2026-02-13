7d5395643259742f73b72f3759f6c61b519be652
skillsstackpro <rohankataria48@gmail.com>
2026-02-13 22:35:59 +0500

    work on actionstack

--- src/ActionStack.java
+++ src/ActionStack.java
@@ -1,20 +1,23 @@
-
-class ActionStack {
-    String[] stack = new String[50];
-    int top = -1;
+public class ActionStack {
+    private String[] stack = new String[50];
+    private int top = -1;
 
     public void push(String action) {
-        if (top < stack.length - 1)
+        if (top < stack.length - 1) {
             stack[++top] = action;
+        } else {
+            System.out.println("History Full! Oldest logs might be lost.");
+        }
     }
 
-    public void display() {
-        if (top == -1) {
-            System.out.println("No history.");
-            return;
-        }
-        for (int i = top; i >= 0; i--) {
-            System.out.println(stack[i]);
-        }
-    }
+//    public void display() {
+//        if (top == -1) {
+//            System.out.println("No recent history.");
+//            return;
+//        }
+//        System.out.println("\n--- Recent Actions (Stack LIFO) ---");
+//        for (int i = top; i >= 0; i--) {
+//            System.out.println(stack[i]);
+//        }
+//    }
 }

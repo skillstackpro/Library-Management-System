f1fdce52d2f91d54718ca5f614bb490f35e311c0
skillsstackpro <rohankataria48@gmail.com>
2026-02-15 13:46:37 +0500

    work on librarydatabase.

--- src/LibraryDatabase.java
+++ src/LibraryDatabase.java
@@ -1,22 +1,22 @@
-class LibraryDatabase {
-    Book[] books = new Book[100];
-    int count = 0;
+public class LibraryDatabase {
+    private Book[] books = new Book[100];
+    private int count = 0;
 
-    // ADD BOOK (O(1))
+    // ADD BOOK - Inserts and Sorts automatically
     public void addBook(int id, String title, String author, int copies) {
         if (count == books.length) {
             System.out.println("Library is full!");
             return;
         }
         books[count++] = new Book(id, title, author, copies);
-        sortBooks();
+        sortBooks(); // Keeping data sorted for Binary Search
     }
 
-    // BUBBLE SORT (O(n^2))
+    // BUBBLE SORT - O(n^2)
     public void sortBooks() {
         for (int i = 0; i < count - 1; i++) {
             for (int j = 0; j < count - i - 1; j++) {
-                if (books[j].id > books[j + 1].id) {
+                if (books[j].getId() > books[j + 1].getId()) { // Using Getter
                     Book temp = books[j];
                     books[j] = books[j + 1];
                     books[j + 1] = temp;
@@ -25,30 +25,31 @@ class LibraryDatabase {
         }
     }
 
-    // BINARY SEARCH (O(log n))
+    // BINARY SEARCH - O(log n)
     public int binarySearch(int id) {
         int left = 0;
-        int right = count - 1; // [cite: 10]
+        int right = count - 1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (books[mid].id == id)
+            if (books[mid].getId() == id)
                 return mid;
-            else if (books[mid].id < id)
-                left = mid + 1; // [cite: 12]
+            else if (books[mid].getId() < id)
+                left = mid + 1;
             else
                 right = mid - 1;
         }
         return -1;
     }
 
-    // DELETE BOOK (O(n))
+    // DELETE BOOK
     public boolean deleteBook(int id) {
         int index = binarySearch(id);
         if (index == -1)
             return false;
         for (int i = index; i < count - 1; i++) {
-            books[i] = books[i + 1]; // [cite: 17]
+            books[i] = books[i + 1];
         }
+        books[count - 1] = null;
         count--;
         return true;
     }
@@ -58,14 +59,15 @@ class LibraryDatabase {
             return books[index];
         return null;
     }
-    public void displayAll() {
-        if (count == 0) {
-            System.out.println("Library is empty.");
-            return;
-        }
-        System.out.println("\nID\tTitle\t\tCopies");
-        for (int i = 0; i < count; i++) {
-            System.out.println(books[i].id + "\t" + books[i].title + "\t\t" + books[i].copies);
-        }
-    }
+
+//    public void displayAll() {
+//        if (count == 0) {
+//            System.out.println("Library is empty.");
+//            return;
+//        }
+//        System.out.println("\nID\tTitle\t\tAuthor\t\tCopies");
+//        for (int i = 0; i < count; i++) {
+//            System.out.println(books[i].getId() + "\t" + books[i].getTitle() + "\t\t" + books[i].getAuthor() + "\t\t" + books[i].getCopies());
+//        }
+//    }
 }

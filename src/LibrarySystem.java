03693e3cf73c053a93184192e235ef552f7ac9d4
skillsstackpro <rohankataria48@gmail.com>
2026-02-16 07:41:13 +0500

    work on librarysytem

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -3,24 +3,29 @@ import java.util.Scanner;
 public class LibrarySystem {
 
     static LibraryDatabase db = new LibraryDatabase();
-    static WaitlistQueue waitlist = new WaitlistQueue();
+    static WaitListQueue waitlist = new WaitListQueue();
     static ActionStack history = new ActionStack();
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // Pre-loading some books for testing
+        db.addBook(101, "Java Programming", "James Gosling", 5);
+        db.addBook(102, "Data Structures", "Robert Lafore", 2);
+
         while (true) {
-            System.out.println("\n=== LIBRARY LOGIN SYSTEM ===");
+            System.out.println("\n=== LIBRARY MANAGEMENT SYSTEM ===");
             System.out.println("1. Login as Admin");
             System.out.println("2. Login as Student");
             System.out.println("3. Exit System");
             System.out.print("Enter choice: ");
+
             int loginChoice = sc.nextInt();
-            sc.nextLine(); // consume newline
+            sc.nextLine(); // Fix scanner bug
 
             if (loginChoice == 1) {
-                System.out.print("Enter Admin Password: ");
+                System.out.print("Enter Admin Password (admin123): ");
                 String pass = sc.nextLine();
-                if (pass.equals("admin123")) { // Simple password check
+                if (pass.equals("admin123")) {
                     adminMenu();
                 } else {
                     System.out.println("Wrong password!");
@@ -28,10 +33,9 @@ public class LibrarySystem {
             } else if (loginChoice == 2) {
                 System.out.print("Enter Student Name: ");
                 String studentName = sc.nextLine();
-                System.out.println("Welcome, " + studentName);
                 studentMenu(studentName);
             } else if (loginChoice == 3) {
-                System.out.println("Shutting down...");
+                System.out.println("Exiting... Have a nice day!");
                 break;
             } else {
                 System.out.println("Invalid choice.");
@@ -40,7 +44,6 @@ public class LibrarySystem {
         sc.close();
     }
 
-
     public static void adminMenu() {
         while (true) {
             System.out.println("\n--- ADMIN DASHBOARD ---");
@@ -48,46 +51,48 @@ public class LibrarySystem {
             System.out.println("2. Delete Book");
             System.out.println("3. Process Waitlist");
             System.out.println("4. View Waitlist");
-            System.out.println("5. View Global History");
+            System.out.println("5. View History");
             System.out.println("6. Display All Books");
             System.out.println("7. Logout");
             System.out.print("Admin Choice: ");
+
             int choice = sc.nextInt();
             sc.nextLine();
 
             switch (choice) {
-                case 1: // Add Book
-                    System.out.print("ID: ");
+                case 1:
+                    System.out.print("Enter ID: ");
                     int id = sc.nextInt();
                     sc.nextLine();
-                    System.out.print("Title: ");
+                    System.out.print("Enter Title: ");
                     String title = sc.nextLine();
-                    System.out.print("Author: ");
+                    System.out.print("Enter Author: ");
                     String author = sc.nextLine();
-                    System.out.print("Copies: ");
+                    System.out.print("Enter Copies: ");
                     int copies = sc.nextInt();
                     db.addBook(id, title, author, copies);
-                    history.push("Admin Added Book ID " + id);
+                    history.push("Admin added book: " + title);
                     break;
                 case 2:
                     System.out.print("Enter ID to delete: ");
                     int did = sc.nextInt();
-                    if (db.deleteBook(did))
+                    if (db.deleteBook(did)) {
                         System.out.println("Deleted successfully.");
-                    else
+                        history.push("Admin deleted book ID: " + did);
+                    } else {
                         System.out.println("Book not found.");
-                    history.push("Admin Deleted Book ID " + did);
+                    }
                     break;
-                case 3: // Process Waitlist
+                case 3:
                     waitlist.processNextStudent();
                     break;
-                case 4: // View Waitlist
+                case 4:
                     waitlist.viewWaitlist();
                     break;
-                case 5: // History
+                case 5:
                     history.display();
                     break;
-                case 6: // Display
+                case 6:
                     db.displayAll();
                     break;
                 case 7:
@@ -98,78 +103,71 @@ public class LibrarySystem {
         }
     }
 
-    // --- STUDENT MENU (Limited Access) ---
     public static void studentMenu(String studentName) {
         while (true) {
             System.out.println("\n--- STUDENT DASHBOARD (" + studentName + ") ---");
             System.out.println("1. Search Book");
-            System.out.println("2. Rent (Issue) Book");
+            System.out.println("2. Rent Book");
             System.out.println("3. Return Book");
-            System.out.println("4. View Library History");
-            System.out.println("5. Display Available Books");
-            System.out.println("6. Logout");
+            System.out.println("4. View Available Books");
+            System.out.println("5. Logout");
             System.out.print("Student Choice: ");
+
             int choice = sc.nextInt();
+            sc.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.print("Search ID: ");
+                    System.out.print("Enter Book ID to Search: ");
                     int sid = sc.nextInt();
                     int idx = db.binarySearch(sid);
                     if (idx != -1) {
                         Book b = db.getBook(idx);
-                        System.out.println("Found: " + b.title + " by " + b.author);
+                        System.out.println("Found: " + b.getTitle() + " by " + b.getAuthor() + " (Copies: " + b.getCopies() + ")");
                     } else {
-                        System.out.println("Not Found.");
+                        System.out.println("Book not found.");
                     }
                     break;
-                case 2: // Rent Book (Issue)
-                    System.out.print("Enter ID to Rent: ");
+                case 2:
+                    System.out.print("Enter Book ID to Rent: ");
                     int iid = sc.nextInt();
                     int iidx = db.binarySearch(iid);
                     if (iidx != -1) {
                         Book b = db.getBook(iidx);
-                        if (b.copies > 0) {
-                            b.copies--;
+                        if (b.getCopies() > 0) {
+                            b.setCopies(b.getCopies() - 1); // Encapsulation Use
                             System.out.println("Book Rented Successfully!");
-                            history.push("Student " + studentName + " Rented ID " + iid);
+                            history.push(studentName + " rented book ID: " + iid);
                         } else {
-                            System.out.print("Out of stock. Join waitlist? (1 for Yes): ");
+                            System.out.print("Out of stock! Join waitlist? (1 for Yes): ");
                             int w = sc.nextInt();
-                            if (w == 1) {
-                                waitlist.addStudent(studentName);
-                            }
+                            if (w == 1) waitlist.addStudent(studentName);
                         }
                     } else {
                         System.out.println("Book not found.");
                     }
                     break;
-                case 3: // Return Book
-                    System.out.print("Enter ID to Return: ");
+                case 3:
+                    System.out.print("Enter Book ID to Return: ");
                     int rid = sc.nextInt();
                     int ridx = db.binarySearch(rid);
                     if (ridx != -1) {
-                        db.getBook(ridx).copies++;
-                        System.out.println("Book Returned.");
-                        history.push("Student " + studentName + " Returned ID " + rid);
+                        Book b = db.getBook(ridx);
+                        b.setCopies(b.getCopies() + 1); // Encapsulation Use
+                        System.out.println("Book Returned. Thank you!");
+                        history.push(studentName + " returned book ID: " + rid);
                     } else {
-                        System.out.println("Book not found in library system.");
+                        System.out.println("This book does not belong to our library.");
                     }
                     break;
-                case 4: // View History
-                    history.display();
-                    break;
-                case 5: // Display All
-                    db.displayAll();
-                    break;
-                case 6:
-                    return; // Returns to Login Menu
-                default:
-                    System.out.println("Invalid choice.");
+//                case 4:
+//                    db.displayAll();
+//                    break;
+//                case 5:
+//                    return;
+//                default:
+//                    System.out.println("Invalid choice.");
             }
         }
     }
 }
-
-
-
